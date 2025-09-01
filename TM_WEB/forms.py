621024f5c83c90a77d7f4dbe004d233dc6f00b21
Андрей <andreyv006@mail.ru>
2025-09-01 21:47:58 +0100

    update

--- TM_WEB/forms.py
+++ TM_WEB/forms.py
@@ -1,1 +1,26 @@
 from django import forms
+from .models import ImageFeed, AudioFeed
+
+
+class ImageFeedForm(forms.ModelForm):
+    class Meta:
+        model = ImageFeed
+        fields = ['image']
+        widgets = {
+            'image': forms.FileInput(attrs={'accept': 'image/*'}),
+        }
+        help_texts = {
+            'image': 'Upload an image file.',
+        }
+
+
+class AudioFeedForm(forms.ModelForm):
+    class Meta:
+        model = AudioFeed
+        fields = ['audio']
+        widgets = {
+            'audio': forms.FileInput(attrs={'accept': 'audio/*'}),
+        }
+        help_texts = {
+            'audio': 'Upload an audio file.',
+        }
