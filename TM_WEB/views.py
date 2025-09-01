9307f765a0434d43d05b6fac6ae548dccf7d4910
Андрей <andreyv006@mail.ru>
2025-09-01 21:46:20 +0100

    added function for audio_to_image process

--- TM_WEB/views.py
+++ TM_WEB/views.py
@@ -2,17 +2,37 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+
+from TM_WEB.forms import ImageFeedForm, AudioFeedForm
+from .models import AudioFeed
+from .functions import audio_to_image as a_to_i
 # from .models import ImageFeed
 # from .utils import process_image
 # from .forms import ImageFeedForm
 
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
 
 def audio_to_image(request):
-    return render(request, 'audio_to_image.html')
+    if request.method == 'POST':
+        print(request.FILES)
+        form = AudioFeedForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(request.FILES['audio'].name)
+            audio_feed = form.save(commit=False)
+            audio_feed.save()
+            audio = AudioFeed.objects.get(audio='audio/' + request.FILES['audio'].name)
+            audio.processed_to_image = a_to_i('../SiteforTM/media/audio/' + request.FILES['audio'].name, request.FILES['audio'].name)
+            print(AudioFeed.objects.get(audio='audio/' + request.FILES['audio'].name))
+            audio.save()
+
+    else:
+        form = AudioFeedForm()
+    feeds = AudioFeed.objects.all()
+    print([f.audio.url for f in feeds])
+    return render(request, 'audio_to_image.html', {'form': form, 'feeds': feeds})
 
 
 def image_to_audio(request):
@@ -21,7 +41,7 @@ def image_to_audio(request):
 
 @login_required
 def audio_from_image(request):
-    return redirect('TM_WEB:audio_from_image')
+    return render(request, 'audio_from_image.html')
 
 
 @login_required
@@ -31,5 +51,5 @@ def audio_insert_in_image(request):
 
 @login_required
 def process_image_feed(request, feed_id):
-    return redirect('TM_WEB:dashboard')
+    return render(request, 'process_image_feed.html')
 
