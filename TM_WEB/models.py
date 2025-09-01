f41a3c0af72c30c03a7f87ff3c0bc7b3e1cffbe9
Андрей <andreyv006@mail.ru>
2025-09-01 17:40:47 +0100

    Basic models

--- TM_WEB/models.py
+++ TM_WEB/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 from django.conf import settings
+
+
+class ImageFeed(models.Model):
+    image = models.ImageField(upload_to='images/')
+    processed_to_audio = models.FileField(upload_to='audio/')
+
+class AudioFeed(models.Model):
+    audio = models.FileField(upload_to='audio/')
+    processed_to_image = models.ImageField(upload_to='images/')
+
+class ImageAudioFeed(models.Model):
+    image = models.ImageField(upload_to='images/')
+    audio = models.FileField(upload_to='audio/')
+    processed_image = models.ImageField(upload_to='images/')
+
+class AudioImageFeed(models.Model):
+    image = models.ImageField(upload_to='images/')
+    processed_to_audio = models.FileField(upload_to='audio/')
