fce12a3bc83090983929a23bc3bd8b29f5ed3b49
Андрей <andreyv006@mail.ru>
2025-09-01 21:44:11 +0100

    update for media

--- SiteforTM/urls.py
+++ SiteforTM/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('TM_WEB/', include('TM_WEB.urls')),
+    path('', RedirectView.as_view(url='/TM_WEB/', permanent=True)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
