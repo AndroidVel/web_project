ca495635c2a1df0dad692699ce98ed60be451de9
Андрей <andreyv006@mail.ru>
2025-09-05 12:08:51 +0100

    added new models for database for other pages

--- TM_WEB/models.py
+++ TM_WEB/models.py
@@ -3,18 +3,22 @@ from django.conf import settings
 
 
 class ImageFeed(models.Model):
+    name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
     processed_to_audio = models.FileField(upload_to='audio/')
 
 class AudioFeed(models.Model):
+    name = models.CharField(max_length=100)
     audio = models.FileField(upload_to='audio/')
     processed_to_image = models.ImageField(upload_to='images/')
 
 class ImageAudioFeed(models.Model):
+    name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
     audio = models.FileField(upload_to='audio/')
     processed_image = models.ImageField(upload_to='images/')
 
 class AudioImageFeed(models.Model):
+    name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/')
     processed_to_audio = models.FileField(upload_to='audio/')
